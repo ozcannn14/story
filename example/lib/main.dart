6596f0920cba3ab590f5633561584aff64cf2545
ozcannn14 <furkann.ozcn@gmail.com>
2021-08-17 09:51:38 +0300

    add dissmiss

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/material.dart';
+
 import 'package:story/story_page_view/story_page_view.dart';
 
 void main() {
@@ -11,12 +15,20 @@ class UserModel {
   final List<StoryModel> stories;
   final String userName;
   final String imageUrl;
+  final id = UniqueKey();
 }
 
 class StoryModel {
-  StoryModel(this.imageUrl);
+  final storyId = UniqueKey();
+  final String title;
+  String? imageUrl;
 
-  final String imageUrl;
+  String get nextVehicleUrl =>
+      'https://source.unsplash.com/collection/1989985/${Random().nextInt(20) + 400}x${Random().nextInt(20) + 800}';
+
+  StoryModel({required this.title, this.imageUrl}) {
+    imageUrl = nextVehicleUrl;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -35,23 +47,101 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final sampleUsers = [
+  UserModel([
+    StoryModel(title: "STORY-1"),
+    StoryModel(title: "STORY-2"),
+    StoryModel(title: "STORY-3"),
+    StoryModel(title: "STORY-4"),
+  ], "User1",
+      "https://images.unsplash.com/photo-1609262772830-0decc49ec18c?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+  UserModel([
+    StoryModel(title: "STORY-2.1"),
+  ], "User2",
+      "https://source.unsplash.com/collection/1989985/${Random().nextInt(20) + 400}x${Random().nextInt(20) + 800}"),
+  UserModel([
+    StoryModel(title: "STORY-3.1"),
+    StoryModel(title: "STORY-3.2"),
+    StoryModel(title: "STORY-3.3"),
+  ], "User3",
+      "https://source.unsplash.com/collection/1989985/${Random().nextInt(20) + 400}x${Random().nextInt(20) + 800}"),
+  UserModel([
+    StoryModel(title: "STORY-3.1"),
+    StoryModel(title: "STORY-3.2"),
+    StoryModel(title: "STORY-3.3"),
+  ], "User4",
+      "https://source.unsplash.com/collection/1989985/${Random().nextInt(20) + 400}x${Random().nextInt(20) + 800}"),
+  UserModel([
+    StoryModel(title: "STORY-3.1"),
+    StoryModel(title: "STORY-3.2"),
+    StoryModel(title: "STORY-3.3"),
+  ], "User5",
+      "https://source.unsplash.com/collection/1989985/${Random().nextInt(20) + 100}x${Random().nextInt(20) + 200}"),
+];
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final padding = MediaQuery.of(context).padding;
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('show stories'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return StoryPage();
-                },
+      body: SingleChildScrollView(
+        padding: padding,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text('Dismissible', style: TextStyle(fontSize: 24)),
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, int i) => StoryWidget(index: i),
+                separatorBuilder: (_, int i) => SizedBox(width: 10),
+                itemCount: sampleUsers.length,
               ),
-            );
-          },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StoryWidget extends StatelessWidget {
+  final int index;
+
+  const StoryWidget({Key? key, required this.index}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          TransparentRoute(
+            backgroundColor: Colors.black.withOpacity(0.35),
+            builder: (_) => StoryPage(
+              startIndex: index,
+            ),
+          ),
+        );
+      },
+      child: Hero(
+        tag: sampleUsers[index].id,
+        child: Container(
+          height: 120,
+          width: 88,
+          padding: const EdgeInsets.all(8),
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.bottomLeft,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(sampleUsers[index].imageUrl),
+            ),
+          ),
         ),
       ),
     );
@@ -59,7 +149,11 @@ class MyHomePage extends StatelessWidget {
 }
 
 class StoryPage extends StatefulWidget {
-  StoryPage({Key? key}) : super(key: key);
+  final int startIndex;
+  StoryPage({
+    Key? key,
+    required this.startIndex,
+  }) : super(key: key);
 
   @override
   _StoryPageState createState() => _StoryPageState();
@@ -67,33 +161,6 @@ class StoryPage extends StatefulWidget {
 
 class _StoryPageState extends State<StoryPage> {
   late ValueNotifier<IndicatorAnimationCommand> indicatorAnimationController;
-  final sampleUsers = [
-    UserModel([
-      StoryModel(
-          "https://images.unsplash.com/photo-1601758228041-f3b2795255f1?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-      StoryModel(
-          "https://images.unsplash.com/photo-1609418426663-8b5c127691f9?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-      StoryModel(
-          "https://images.unsplash.com/photo-1609444074870-2860a9a613e3?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Nnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-      StoryModel(
-          "https://images.unsplash.com/photo-1609504373567-acda19c93dc4?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-    ], "User1",
-        "https://images.unsplash.com/photo-1609262772830-0decc49ec18c?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-    UserModel([
-      StoryModel(
-          "https://images.unsplash.com/photo-1609439547168-c973842210e1?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Nnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-    ], "User2",
-        "https://images.unsplash.com/photo-1601758125946-6ec2ef64daf8?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMjN8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-    UserModel([
-      StoryModel(
-          "https://images.unsplash.com/photo-1609421139394-8def18a165df?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMDl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-      StoryModel(
-          "https://images.unsplash.com/photo-1609377375732-7abb74e435d9?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODJ8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-      StoryModel(
-          "https://images.unsplash.com/photo-1560925978-3169a42619b2?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMjF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-    ], "User3",
-        "https://images.unsplash.com/photo-1609127102567-8a9a21dc27d8?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOTh8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-  ];
 
   @override
   void initState() {
@@ -111,10 +178,11 @@ class _StoryPageState extends State<StoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       body: StoryPageView(
+        initialPage: widget.startIndex,
+        indicatorPadding: EdgeInsets.only(top: 30, left: 10, right: 10),
         itemBuilder: (context, pageIndex, storyIndex) {
-          final user = sampleUsers[pageIndex];
-          final story = user.stories[storyIndex];
           return Stack(
             children: [
               Positioned.fill(
@@ -122,86 +190,94 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Positioned.fill(
                 child: Image.network(
-                  story.imageUrl,
+                  sampleUsers[pageIndex].stories[storyIndex].nextVehicleUrl,
                   fit: BoxFit.cover,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 44, left: 8),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 32,
-                      width: 32,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(user.imageUrl),
-                          fit: BoxFit.cover,
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50, left: 10, right: 10),
+                  child: Row(
+                    children: [
+                      Hero(
+                        tag: sampleUsers[pageIndex].id,
+                        child: Container(
+                          height: 32,
+                          width: 32,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image:
+                                  NetworkImage(sampleUsers[pageIndex].imageUrl),
+                              fit: BoxFit.cover,
+                            ),
+                            shape: BoxShape.circle,
+                          ),
                         ),
-                        shape: BoxShape.circle,
                       ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      user.userName,
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(
+                        width: 8,
                       ),
-                    ),
-                  ],
+                      Text(
+                        sampleUsers[pageIndex].userName,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
           );
         },
         gestureItemBuilder: (context, pageIndex, storyIndex) {
-          return Stack(children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 32),
-                child: IconButton(
-                  padding: EdgeInsets.zero,
-                  color: Colors.white,
-                  icon: Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+          return SafeArea(
+            child: Stack(children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 32),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    color: Colors.white,
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
                 ),
               ),
-            ),
-            if (pageIndex == 0)
-              Center(
-                child: ElevatedButton(
-                  child: Text('show modal bottom sheet'),
-                  onPressed: () async {
-                    indicatorAnimationController.value =
-                        IndicatorAnimationCommand.pause;
-                    await showModalBottomSheet(
-                      context: context,
-                      builder: (context) => SizedBox(
-                        height: MediaQuery.of(context).size.height / 2,
-                        child: Padding(
-                          padding: EdgeInsets.all(24),
-                          child: Text(
-                            'Look! The indicator is now paused\n\n'
-                            'It will be coutinued after closing the modal bottom sheet.',
-                            style: Theme.of(context).textTheme.headline5,
-                            textAlign: TextAlign.center,
+              /*  if (pageIndex == 0)
+                Center(
+                  child: ElevatedButton(
+                    child: Text('show modal bottom sheet'),
+                    onPressed: () async {
+                      indicatorAnimationController.value =
+                          IndicatorAnimationCommand.pause;
+                      await showModalBottomSheet(
+                        context: context,
+                        builder: (context) => SizedBox(
+                          height: MediaQuery.of(context).size.height / 2,
+                          child: Padding(
+                            padding: EdgeInsets.all(24),
+                            child: Text(
+                              'Look! The indicator is now paused\n\n'
+                              'It will be coutinued after closing the modal bottom sheet.',
+                              style: Theme.of(context).textTheme.headline5,
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                    indicatorAnimationController.value =
-                        IndicatorAnimationCommand.resume;
-                  },
-                ),
-              ),
-          ]);
+                      );
+                      indicatorAnimationController.value =
+                          IndicatorAnimationCommand.resume;
+                    },
+                  ),
+                ), */
+            ]),
+          );
         },
         indicatorAnimationController: indicatorAnimationController,
         initialStoryIndex: (pageIndex) {
@@ -217,6 +293,7 @@ class _StoryPageState extends State<StoryPage> {
         onPageLimitReached: () {
           Navigator.pop(context);
         },
+        indicatorDuration: Duration(seconds: 1000),
       ),
     );
   }
--- lib/story_page_view/story_page_view.dart
+++ lib/story_page_view/story_page_view.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -100,60 +101,67 @@ class _StoryPageViewState extends State<StoryPageView> {
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: widget.backgroundColor,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: widget.pageLength,
-        itemBuilder: (context, index) {
-          final isLeaving = (index - currentPageValue) <= 0;
-          final t = (index - currentPageValue);
-          final rotationY = lerpDouble(0, 30, t as double)!;
-          final maxOpacity = 0.8;
-          final num opacity =
-              lerpDouble(0, maxOpacity, t.abs())!.clamp(0.0, maxOpacity);
-          final isPaging = opacity != maxOpacity;
-          final transform = Matrix4.identity();
-          transform.setEntry(3, 2, 0.003);
-          transform.rotateY(-rotationY * (pi / 180.0));
-          return Transform(
-            alignment: isLeaving ? Alignment.centerRight : Alignment.centerLeft,
-            transform: transform,
-            child: Stack(
-              children: [
-                _StoryPageFrame.wrapped(
-                  pageLength: widget.pageLength,
-                  storyLength: widget.storyLength(index),
-                  initialStoryIndex: widget.initialStoryIndex?.call(index) ?? 0,
-                  pageIndex: index,
-                  animateToPage: (index) {
-                    pageController!.animateToPage(index,
-                        duration: Duration(milliseconds: 500),
-                        curve: Curves.ease);
-                  },
-                  isCurrentPage: currentPageValue == index,
-                  isPaging: isPaging,
-                  onPageLimitReached: widget.onPageLimitReached,
-                  itemBuilder: widget.itemBuilder,
-                  gestureItemBuilder: widget.gestureItemBuilder,
-                  indicatorDuration: widget.indicatorDuration,
-                  indicatorPadding: widget.indicatorPadding,
-                  indicatorAnimationController:
-                      widget.indicatorAnimationController,
-                ),
-                if (isPaging && !isLeaving)
-                  Positioned.fill(
-                    child: Opacity(
-                      opacity: opacity as double,
-                      child: ColoredBox(
-                        color: Colors.black87,
+    return DismissiblePage(
+      onDismiss: () => Navigator.of(context).pop(),
+      isFullScreen: true,
+      direction: DismissDirection.down,
+      child: ColoredBox(
+        color: widget.backgroundColor,
+        child: PageView.builder(
+          controller: pageController,
+          itemCount: widget.pageLength,
+          itemBuilder: (context, index) {
+            final isLeaving = (index - currentPageValue) <= 0;
+            final t = (index - currentPageValue);
+            final rotationY = lerpDouble(0, 30, t as double)!;
+            final maxOpacity = 0.8;
+            final num opacity =
+                lerpDouble(0, maxOpacity, t.abs())!.clamp(0.0, maxOpacity);
+            final isPaging = opacity != maxOpacity;
+            final transform = Matrix4.identity();
+            transform.setEntry(3, 2, 0.003);
+            transform.rotateY(-rotationY * (pi / 180.0));
+            return Transform(
+              alignment:
+                  isLeaving ? Alignment.centerRight : Alignment.centerLeft,
+              transform: transform,
+              child: Stack(
+                children: [
+                  _StoryPageFrame.wrapped(
+                    pageLength: widget.pageLength,
+                    storyLength: widget.storyLength(index),
+                    initialStoryIndex:
+                        widget.initialStoryIndex?.call(index) ?? 0,
+                    pageIndex: index,
+                    animateToPage: (index) {
+                      pageController!.animateToPage(index,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease);
+                    },
+                    isCurrentPage: currentPageValue == index,
+                    isPaging: isPaging,
+                    onPageLimitReached: widget.onPageLimitReached,
+                    itemBuilder: widget.itemBuilder,
+                    gestureItemBuilder: widget.gestureItemBuilder,
+                    indicatorDuration: widget.indicatorDuration,
+                    indicatorPadding: widget.indicatorPadding,
+                    indicatorAnimationController:
+                        widget.indicatorAnimationController,
+                  ),
+                  if (isPaging && !isLeaving)
+                    Positioned.fill(
+                      child: Opacity(
+                        opacity: opacity as double,
+                        child: ColoredBox(
+                          color: Colors.black87,
+                        ),
                       ),
                     ),
-                  ),
-              ],
-            ),
-          );
-        },
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -321,12 +329,14 @@ class _StoryPageFrameState extends State<_StoryPageFrame>
             ],
           ),
         ),
-        Indicators(
-          storyLength: widget.storyLength,
-          animationController: animationController,
-          isCurrentPage: widget.isCurrentPage,
-          isPaging: widget.isPaging,
-          padding: widget.indicatorPadding,
+        SafeArea(
+          child: Indicators(
+            storyLength: widget.storyLength,
+            animationController: animationController,
+            isCurrentPage: widget.isCurrentPage,
+            isPaging: widget.isPaging,
+            padding: widget.indicatorPadding,
+          ),
         ),
         Gestures(
           animationController: animationController,
